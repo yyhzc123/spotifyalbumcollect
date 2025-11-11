60036b8d2095e73ae94b961426e62cf5a345958e
yyhzc123 <897714549@qq.com>
2025-11-11 17:58:22 +0800

    Fix: Add Spotify image domain to next.config.js

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.scdn.co',
+        port: '',
+        pathname: '/image/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
