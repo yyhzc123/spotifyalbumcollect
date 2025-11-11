0e801a3eb9a25bb18845e3145e83d5cffffb3256
yyhzc123 <897714549@qq.com>
2025-11-11 14:19:55 +0800

    Fix TypeScript type errors

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth"
 import SpotifyProvider from "next-auth/providers/spotify"
+import { JWT } from "next-auth/jwt";
+import { Account, Session, User } from "next-auth";
 
 export const authOptions = {
   // Configure one or more authentication providers
@@ -12,13 +14,13 @@ export const authOptions = {
     // ...add more providers here
   ],
   callbacks: {
-    async jwt({ token, account }) {
+    async jwt({ token, account }: { token: JWT; account: Account | null }): Promise<JWT> {
       if (account) {
         token.accessToken = account.access_token
       }
       return token
     },
-    async session({ session, token, user }) {
+    async session({ session, token, user }: { session: Session; token: JWT; user: User }): Promise<Session> {
       session.accessToken = token.accessToken
       return session
     },
